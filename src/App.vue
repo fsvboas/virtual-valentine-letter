b51a6f100fe859d8b8e5dee1a541ebcd1a620197
fsvboas <fsvboas.dev@gmail.com>
2025-05-27 19:25:19 -0300

    refactor: change app background color

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div class="flex flex-col min-h-screen w-full bg-pink-300 font-medium">
+  <div class="flex flex-col min-h-screen w-full bg-pink-200 font-medium">
     <RouterView />
   </div>
 </template>
