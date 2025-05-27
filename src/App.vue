394b69b19097203d0726a75cd77b1b11225b40fc
fsvboas <fsvboas.dev@gmail.com>
2025-05-27 20:58:23 -0300

    feat: add a background-image to app

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div class="flex flex-col min-h-screen w-full bg-pink-200 font-medium">
+  <div
+    class="flex flex-col min-h-screen w-full bg-[url(../../public/image-background.webp)] bg-cover bg-no-repeat font-medium"
+  >
     <RouterView />
   </div>
 </template>
