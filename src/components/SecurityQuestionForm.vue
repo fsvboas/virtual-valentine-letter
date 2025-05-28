bc5d0b03fca21361e2312fa94271475209c01d9f
fsvboas <fsvboas.dev@gmail.com>
2025-05-27 21:45:52 -0300

    feat: create initial layout to security-question-form component

---
+++ src/components/SecurityQuestionForm.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const inputValue = ref<string>('')
+</script>
+
+<template>
+  <div class="flex flex-col w-full h-fit p-4 bg-white border-2 border-[#f8cfa3] rounded space-y-2">
+    <span class="text-sm text-center"
+      >Antes de continuar, confirme que esta carta é para você.</span
+    >
+    <form @submit.prevent="">
+      <label class="text-xs">Em qual dia nos conhecemos?</label>
+      <div class="flex flex-row space-x-2">
+        <input
+          v-model="inputValue"
+          class="bg-white border-2 border-[#f8cfa3] w-full p-1 rounded-md"
+          type="text"
+        />
+        <button type="submit" class="bg-[#f8cfa3] p-2 rounded-md hover:cursor-pointer text-sm">
+          Validar
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import LetterEnvelope from '@/components/LetterEnvelope.vue'
+import SecurityQuestionForm from '@/components/SecurityQuestionForm.vue'
 </script>
 
 <template>
-  <main class="flex flex-col items-center justify-center min-h-screen">
-    <LetterEnvelope />
+  <main class="flex flex-col items-center justify-center min-h-screen h-full">
+    <div class="flex flex-col justify-center items-center space-y-4 max-w-[300px]">
+      <LetterEnvelope />
+      <SecurityQuestionForm />
+    </div>
   </main>
 </template>
