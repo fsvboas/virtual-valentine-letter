4720b34a19e33126d89302202fce4d95d60ff2e0
fsvboas <fsvboas.dev@gmail.com>
2025-06-12 15:29:17 -0300

    refactor: remove SecurityQuestionForm component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
 import LetterEnvelope from '@/components/LetterEnvelope.vue'
-import SecurityQuestionForm from '@/components/SecurityQuestionForm.vue'
 </script>
 
 <template>
   <main class="flex flex-col items-center justify-center min-h-screen h-full">
-    <div class="flex flex-col justify-center items-center space-y-4 max-w-[300px]">
+    <div class="flex flex-col justify-center items-center max-w-[300px]">
       <LetterEnvelope />
-      <SecurityQuestionForm />
     </div>
   </main>
 </template>
