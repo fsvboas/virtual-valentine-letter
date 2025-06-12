42d28c7c3611b8082da7adbede280870cdebb7a6
fsvboas <fsvboas.dev@gmail.com>
2025-06-12 15:29:22 -0300

    feat: create LetterModal and update LetterEnvelope logic

--- src/components/LetterEnvelope.vue
+++ src/components/LetterEnvelope.vue
@@ -1,23 +1,36 @@
 <script setup lang="ts">
+import { useLetterModalStore } from '@/stores/useLetterModalStore'
 import { ref } from 'vue'
+import Letter from './LetterModal.vue'
 
 const isEnvelopeOpen = ref<boolean>(false)
 const isLetterLifted = ref<boolean>(false)
 
-const toggleEnvelope = () => {
+const letterModalStore = useLetterModalStore()
+
+const handleClick = () => {
   if (!isEnvelopeOpen.value) {
     isEnvelopeOpen.value = true
     setTimeout(() => {
       isLetterLifted.value = true
     }, 300)
   }
+  if (isEnvelopeOpen.value && isLetterLifted.value) {
+    letterModalStore.openModal()
+  }
 }
 </script>
 
 <template>
   <div
+    class="bg-black/90 w-full h-full absolute z-60 flex items-center justify-center"
+    v-if="letterModalStore.isModalOpen === true"
+  >
+    <Letter />
+  </div>
+  <div
     class="flex bg-[#ffe4c7] w-[300px] h-[200px] relative cursor-pointer justify-center shadow-2xl"
-    @click="toggleEnvelope"
+    @click="handleClick"
   >
     <div
       :class="[
@@ -36,19 +49,19 @@ const toggleEnvelope = () => {
       className="w-0 h-0 border-t-[100px] border-t-transparent border-b-[100px] border-b-transparent border-l-[150px] border-l-[#f8cfa3] z-30 absolute left-0"
     />
     <div
-      @click="toggleEnvelope"
       :class="[
         'w-[95%] h-full bg-white overflow-hidden absolute duration-300',
         isEnvelopeOpen ? 'z-20' : 'z-0',
         isLetterLifted ? '-mt-20' : '0',
       ]"
     >
-      <p class="text-center p-4">
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-        ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-        ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
+      <p class="text-justify p-4">
+        Yasmin,
+        <br />
+        <br />
+        Esse é o nosso primeiro dia dos namorados. É o meu primeiro da vida, honestamente, mas eu
+        não poderia estar mais contente. Sou imensamente grato por ter te conhecido, ter você ao meu
+        lado é, de longe, a minha maior realização dos últimos anos.
       </p>
     </div>
   </div>
---
+++ src/components/LetterModal.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { useLetterModalStore } from '@/stores/useLetterModalStore'
+import { X } from 'lucide-vue-next'
+
+const letterModalStore = useLetterModalStore()
+</script>
+
+<template>
+  <div class="bg-white absolute z-70 max-w-[500px] h-fit p-4 flex flex-col">
+    <button
+      class="flex self-end cursor-pointer hover:text-red-500 duration-200 p-px"
+      @click="letterModalStore.closeModal"
+    >
+      <X />
+    </button>
+    <p class="text-justify p-4 max-md:text-sm">
+      Yasmin,
+      <br />
+      <br />
+      Esse é o nosso primeiro dia dos namorados. É o meu primeiro da vida, honestamente, mas eu não
+      poderia estar mais contente. Sou imensamente grato por ter te conhecido, ter você ao meu lado
+      é, de longe, a minha maior realização dos últimos anos.
+      <br />
+      <br />
+      Seu jeitinho, sua alegria e sua força de vontade me motiva todos os dias. Me faz querer ser
+      mais, me faz querer correr atrás dos meus próprios objetivos. Algumas coisas que eu já até
+      havia desistido, mas que depois de te conhecer, me fez querer correr atrás novamente.
+      <br />
+      <br />
+      Espero que a nossa história continue a se desenrolar com muitos momentos felizes, e que nunca
+      nos falte força para enfrentarmos juntos qualquer desafio que surgir.
+      <br />
+      <br />
+      Minha gatinha, saiba que torço por você todos os dias. Conte comigo para qualquer coisa na sua
+      vida. Farei de tudo para estar aqui, sempre ao seu lado.
+      <br />
+      <br />
+      <span class="text-2xl text-red-700">Eu te amo!</span>
+    </p>
+  </div>
+</template>
