3e5137f97a696851f4e5050284996a22db4883ff
fsvboas <fsvboas.dev@gmail.com>
2025-06-12 15:27:31 -0300

    feat: create useLetterModalStore pinia store

---
+++ src/stores/useLetterModalStore.ts
@@ -0,0 +1,15 @@
+import { defineStore } from 'pinia'
+
+export const useLetterModalStore = defineStore('letterModal', {
+  state: () => ({
+    isModalOpen: false,
+  }),
+  actions: {
+    openModal() {
+      this.isModalOpen = true
+    },
+    closeModal() {
+      this.isModalOpen = false
+    },
+  },
+})
