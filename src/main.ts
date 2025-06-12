f73154fa455adfb29e768ca5c25880dc3aa53ac5
fsvboas <fsvboas.dev@gmail.com>
2025-06-12 15:27:08 -0300

    chore: add pinia lib

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import './main.css'
 
+import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+const pinia = createPinia()
 const app = createApp(App)
 
+app.use(pinia)
 app.use(router)
-
 app.mount('#app')
